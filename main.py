7a9bb8142363fbbb8f1729a0e8fe72d197a4a2cc
fallnomega <fallnomega@gmail.com>
2023-03-31 10:44:31 -0700

    setup viewing individual blog posts

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template
 
 
@@ -5,7 +6,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    # print(num)
+    blog_url = 'https://api.npoint.io/1fdb956d180291f3f459'
+    blog_get = requests.get(url=blog_url)
+    the_blog = blog_get.json()
+    return render_template("index.html", all_posts=the_blog)
+
+@app.route('/blog/<num>')
+def get_blog(num):
+    blog_url='https://api.npoint.io/1fdb956d180291f3f459'
+    blog_get = requests.get(url=blog_url)
+    the_blog=blog_get.json()
+    return render_template('blog.html', all_posts=the_blog,blog_id=int(num))
 
 if __name__ == "__main__":
     app.run(debug=True)
